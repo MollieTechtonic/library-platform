1ad6c358db9db3d31193bff3b2e20a5128b81022
Mollie Chacon <mollie.chacon@techtonic.com>
2018-10-04 16:42:41 -0600

    editBook complete and final tweaks/code cleanup

--- js/book.js
+++ js/book.js
@@ -1,5 +1,3 @@
-// not creating an instance here but on the fly inside engine.js
-
 function Book(title, author, numPages, pubDate)
 {
   this.title = title;
@@ -9,10 +7,24 @@ function Book(title, author, numPages, pubDate)
 
 }
 
-// method that takes in a book obj
 Book.prototype.editBook = function(oBook)
 {
+// this method assumes that a title will ALWAYS be given.... not sure if it works
+  // for (var i=0; i<this.bookshelf.length; i++) {
+  //  if (oBook.title === this.bookshelf[i].title) {
+  //    this.bookshelf[i].author = oBook.author;
+  //    this.bookshelf[i].numPages = oBook.numPages;
+  //    this.bookshelf[i].pubDate = oBook.pubDate;
+  //  }
+  // }
 
+// this method will change any number of attributes, even 0
+  this.title = oBook.title || this.title;
+  this.author = oBook.author || this.author;
+  this.numPages = oBook.numPages || this.numPages;
+  this.pubDate = oBook.pubDate || this.pubDate;
+
+  return this;
 };
 
 var book1 = new Book("Harry Potter and the Goblet of Fire", "JK Rowling", 636, "July 8, 2000");
@@ -29,14 +41,16 @@ var book6 = new Book ("What is the What", "Dave Eggers", 475, "October 25, 2006"
 
 var book7 = new Book ("The Glass Castle", "Jeannette Walls", 289, "March 8, 2005");
 
-var book8 = new Book ("Other Book", "Dave Eggers", 475, "October 25, 2006");
+var book8 = new Book ("The Prophet", "Kahlil Gibran", 107, "October 25, 1923");
 
 
 // 3 more books and a book array to test addBooks function
-var book9 = new Book ("Random1", "JK Rowling", 475, "October 25, 2006");
+var book9 = new Book ("Harry Potter and the Half Blood Prince", "JK Rowling", 652, "July 16, 2005");
 
-var book10 = new Book ("Random2", "Agatha Christie", 475, "October 25, 2006");
+var book10 = new Book ("The Pale Horse", "Agatha Christie", 256, "November 6, 1961");
 
-var book11 = new Book ("Random3", "Dave Eggers", 475, "October 25, 2006");
+var book11 = new Book ("You Shall Know Our Velocity", "Dave Eggers", 371, "October 25, 2002");
 
-var bookArray = [book9, book10, book11];
+var book12 = new Book ("Heart of Darkness", "Joseph Conrad", 151, "October 25, 1902")
+
+var bookArray = [book9, book10, book11, book12];
--- js/engine.js
+++ js/engine.js
@@ -1,38 +1,30 @@
 function Library()
 {
   this.bookshelf = new Array();
-  // could also do this.bookshelf = []; ? still an object this way right?
+  // this.bookshelf = [];
+  // what is the benefit of calling new Array() instead of just []?
 };
 
-// Done - GO BACK AND USE TOLOWERCASE()
-Library.prototype.addBook = function(book)
-{
-
-  // check if book is already in my array(bookshelf)
+Library.prototype.addBook = function(book) {
+  // check if book is already in my array(bookshelf) using only lowercase
   for(var i=0; i<this.bookshelf.length; i++) {
-    if (this.bookshelf[i].title === book.title) {
-      //false if not added or already exists
+    if (this.bookshelf[i].title.toLowerCase() === book.title.toLowerCase()) {
+      //false if already exists
       return false;
     }
   }
-  // add book that's coming from book param if title isn't already in arr
+  // else, add book that's coming from book param
   this.bookshelf.push(book);
   // return true if book was added
   return true;
 };
 
-// Done
-Library.prototype.removeBookByTitle = function(title)
-{
-  // loop through books and search for a title that matches the input param
+Library.prototype.removeBookByTitle = function(title) {
+  // loop through books and search for a title that matches the input param using all lowercase
   for (var i=0; i<this.bookshelf.length; i++) {
     // if a match is found,
-    if (this.bookshelf[i].title === title) {
-      // delete that book object
-      // this method leaves a hole in the array
-      // delete this.bookshelf[i];
-
-      // trying splice method - WORKS!!
+    if (this.bookshelf[i].title.toLowerCase() === title.toLowerCase()) {
+      // splice out that book from the bookshelf array
       this.bookshelf.splice(i, 1);
       // return true of book was removed
       return true;
@@ -42,32 +34,29 @@ Library.prototype.removeBookByTitle = function(title)
   return false;
 };
 
-// Done
-Library.prototype.removeBookByAuthor = function(authorName)
-{
+Library.prototype.removeBookByAuthor = function(authorName) {
+  // initialize var that will increment if a book was removed
   var counter = 0;
-  // loop through books and search for an author that matches the input param
+  // loop through books and search for an author that matches the input param in all lowercase
   for (var i=0; i<this.bookshelf.length; i++) {
     // if any books found with author match
-    if (this.bookshelf[i].author === authorName) {
+    if (this.bookshelf[i].author.toLowerCase() === authorName.toLowerCase()) {
       // delete any books that are by that author
       this.bookshelf.splice(i, 1);
-      // common bug with splice: when an item is removed, the item at index i is not what WAS THE NEXT ITEM ON THE LIST. Therefore, each time an item is removed we need to decrement i back one to ensure we are checking EVERY book.
+      // common bug with splice: when an item is removed, the item at index i is now what WAS THE NEXT ITEM ON THE LIST. Therefore, each time an item is removed we need to decrement i back one to ensure we are checking EVERY book.
       i--;
       counter++;
     }
   }
   // return true if book(s) removed; return false if no books match
-  if (counter !== 0) {
+  if (counter > 0) {
     return true;
   } else {
     return false;
   }
 };
 
-// Done
-Library.prototype.getRandomBook = function()
-{
+Library.prototype.getRandomBook = function() {
   // use Math.random to find a random book
   var randomBook = this.bookshelf[Math.floor(Math.random() * this.bookshelf.length)];
   // return random book object (or null if there are no books)
@@ -78,18 +67,15 @@ Library.prototype.getRandomBook = function()
   }
 };
 
-// Done - GO BACK AND USE TO LOWERCASE
-Library.prototype.getBookByTitle = function(titlePiece)
-{
+Library.prototype.getBookByTitle = function(titlePiece) {
   // declare a var for our title matches to push into
   // outside our function for scoping reasons
   var titleMatchArray = [];
-  // loop through all books for a match of the input title param
+  // loop through all books for a match of the input title param using all lowercase
   for (var i=0; i<this.bookshelf.length; i++) {
-    // checking to see the loop runs through each title
-    // console.log(this.bookshelf[i].title);
     // if there is a full/partial match
-    if (this.bookshelf[i].title.indexOf(titlePiece) !== -1) {
+    // note: indexOf returns -1 if not present; if present, first index
+    if (this.bookshelf[i].title.toLowerCase().indexOf(titlePiece.toLowerCase()) > -1) {
     // add to our title array
     titleMatchArray.push(this.bookshelf[i]);
     }
@@ -98,18 +84,14 @@ Library.prototype.getBookByTitle = function(titlePiece)
   return titleMatchArray;
 };
 
-// Done - GO BACK AND USE TO LOWERCASE
-Library.prototype.getBookByAuthor = function(authorName)
-{
+Library.prototype.getBookByAuthor = function(authorName) {
   // declare a var for our author matches to push into
   // outside our function for scopng reasons
   var authorMatchArray = [];
   // loop through all books for a match of the input author param
   for (var i=0; i<this.bookshelf.length; i++) {
-    // check to see the loop runs through each book author
-    // console.log(this.bookshelf[i].author);
     // if there is a full/partial match,
-    if (this.bookshelf[i].author.indexOf(authorName) !== -1) {
+    if (this.bookshelf[i].author.toLowerCase().indexOf(authorName.toLowerCase()) > -1) {
       // add to our author array
       authorMatchArray.push(this.bookshelf[i]);
     }
@@ -118,43 +100,34 @@ Library.prototype.getBookByAuthor = function(authorName)
   return authorMatchArray;
 };
 
-// Done
-Library.prototype.addBooks = function(books)
-{
+Library.prototype.addBooks = function(books) {
   // take in multiple books in the form of an array of book objects and add them to the bookShelf array
-
   // found that "this" here points to my object -
   // inside my forEach, "this" points to the window -
   // therefore, declare var This = this in object context
   var This = this;
-  // log bookshelf length before adding book(s)
+  // capture bookshelf length before adding book(s)
   var currentLength = this.bookshelf.length;
-
   // run established addBook function on each new book object in the argument array
   books.forEach(function(b) {
     This.addBook(b);
   });
-
-  // log new bookshelf length after attempting to add book(s)
+  // capture new bookshelf length after attempting to add book(s)
   var newLength = this.bookshelf.length;
-
-  // return # of new books added to bookshelf
+  // return # of new books added to bookshelf (0 if none added)
   return newLength - currentLength;
 };
 
-// Done BUT.... Definitely a better way to do this...
-Library.prototype.getAuthors = function()
-{
+// Definitely a better way to do this...
+Library.prototype.getAuthors = function() {
   // declare a var to contain all authors
   var allAuthorArray = [];
   // declar a var to contain unique authors
   var uniqueArray = [];
-
-  // loop through bookshelf and add ALL authors to allAuthorArray
+  // loop through bookshelf and add ALL authors to allAuthorArray - inefficient for large library.....
   for (var i=0; i<this.bookshelf.length; i++) {
     allAuthorArray.push(this.bookshelf[i].author);
   }
-
   // loop through allAuthorArray
   for (var j=0; j<allAuthorArray.length; j++) {
     // add to unique array if not already there
@@ -164,12 +137,11 @@ Library.prototype.getAuthors = function()
   }
   // return array of strings of all unique authors; empty array if no books/no authors exist
   return uniqueArray;
-}
+};
 
-
-// Done BUT... Definitely a better way to do this...
-Library.prototype.getRandomAuthorName = function()
-{
+// Definitely a better way to do this... leverage getRandomBook??
+Library.prototype.getRandomAuthorName = function() {
+  // assign a random book obj to a var
   var randomBook = this.bookshelf[Math.floor(Math.random() * this.bookshelf.length)];
 
   // return random author name as a string (or null if there are no books)
@@ -180,13 +152,16 @@ Library.prototype.getRandomAuthorName = function()
   }
 };
 
+
+
 // where we're creating the instance - new Library created that has a bookShelf property (which is an array that eventually will be filled with books.)
 document.addEventListener("DOMContentLoaded", function(e) {
+
   // how we access all our methods
   // WHY CALLED WINDOW.GLIBRARY????????????????
   window.gLibrary = new Library();
 
-  // adding all the seed books from book.js
+  // adding seed books from book.js
   window.gLibrary.addBook(book1);
   window.gLibrary.addBook(book2);
   window.gLibrary.addBook(book3);
